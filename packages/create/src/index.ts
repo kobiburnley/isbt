1658f1bd00574a516f9cdd9fe47444099bda0597
Kobi <qobyyy@gmail.com>
2021-08-20 15:56:15 +0300

    WIP

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,1 +1,12 @@
-export * from './create'
+#!/usr/bin/env node
+
+import { create } from './create'
+import { throwLeft } from 'fp-error'
+;(async () => {
+  try {
+    await throwLeft(() => create(process.env.INIT_CWD ?? process.cwd()))
+  } catch (e) {
+    console.error(e)
+    process.exitCode = 1
+  }
+})()
