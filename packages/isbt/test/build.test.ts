b112c1d66b41d23362b2ef82275f30549ef3c57d
Kobi <qobyyy@gmail.com>
2021-09-10 17:28:17 +0300

    out

--- packages/isbt/test/build.test.ts
+++ packages/isbt/test/build.test.ts
@@ -1,5 +1,8 @@
 import { runIsbtCommandOnFixture } from './runIsbtCommandOnFixture'
 
+jest.useFakeTimers()
+jest.setTimeout(15000)
+
 describe('build', () => {
   it('runs command', async () => {
     await runIsbtCommandOnFixture('workspace-1', 'build')
--- packages/isbt/test/lint.test.ts
+++ packages/isbt/test/lint.test.ts
@@ -1,5 +1,8 @@
 import { runIsbtCommandOnFixture } from './runIsbtCommandOnFixture'
 
+jest.useFakeTimers()
+jest.setTimeout(15000)
+
 describe('lint', () => {
   it('passes when missing files', async () => {
     await runIsbtCommandOnFixture('workspace-1', 'lint . --passWithNoTests')
